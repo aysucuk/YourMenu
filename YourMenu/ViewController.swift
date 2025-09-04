afba634f2c424039af87676a2cfddb7d86b277aa
Aysu Sadikhova <aysu.sadixova@accessbank.az>
2025-09-04 15:18:42 +0400

    Initial Commit

---
+++ YourMenu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YourMenu
+//
+//  Created by Aysu Sadikhova on 04.09.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
